f7b59e25897bcef497610ecb8c422ce5dfdb4d17
Elie Michael Ngandu <eliengandu@wsite.nl>
2020-06-16 22:53:14 +0200

    task and worker

--- WorkQueues.Worker/Program.cs
+++ WorkQueues.Worker/Program.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
 using System.Linq;
 using System.Text;
-using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
+using System.Threading;
 
-namespace RabbitMqReceiver
+namespace WorkQueues.Worker
 {
     class Program
     {
@@ -20,22 +21,29 @@ namespace RabbitMqReceiver
             using var connection = factory.CreateConnection();
             using var channel = connection.CreateModel();
 
-            channel.QueueDeclare(queue: "hello",
-                                 durable: false,
+            channel.QueueDeclare(queue: "task_queue",
+                                 durable: true,
                                  exclusive: false,
                                  autoDelete: false,
                                  arguments: null);
+            channel.BasicQos(0, 1, false);
 
             var consumer = new EventingBasicConsumer(channel);
+
             consumer.Received += (model, ea) =>
             {
                 var body = ea.Body.ToArray();
-                var message = Encoding.UTF8.GetString(body.ToArray());
+                var message = Encoding.UTF8.GetString(body);
                 Console.WriteLine(" [x] Received {0}", message);
+
+                int dots = message.Split('.').Length - 1;
+                Thread.Sleep(dots * 1000);
+
+                Console.WriteLine(" [x] Done");
+                channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
             };
-            channel.BasicConsume(queue: "hello",
-                                 autoAck: true,
-                                 consumer: consumer);
+
+            channel.BasicConsume(queue: "task_queue", autoAck: true, consumer: consumer);
 
             Console.WriteLine(" Press [enter] to exit.");
             Console.ReadLine();
