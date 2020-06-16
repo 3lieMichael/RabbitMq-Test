47fa4a5e476d84c75e4f8e05d2c9c6c9fde59bf7
Elie Michael Ngandu <eliengandu@wsite.nl>
2020-06-16 14:43:03 +0200

    Add project files.

---
+++ RabbitMqSender/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMqSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            for (int i = 0; i < 5; i++)
+            {
+                var message = $"Hello World! - {DateTime.Now.TimeOfDay}";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+
+                Console.WriteLine(" [x] Sent {0}", message);
+
+                Thread.Sleep(15000);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
