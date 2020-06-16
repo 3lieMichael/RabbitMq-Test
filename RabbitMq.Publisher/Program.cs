510da3094f7cc1055999169e6eb28a5d7c56b55c
Elie Michael Ngandu <eliengandu@wsite.nl>
2020-06-16 23:26:55 +0200

    log publisher and subscriber

---
+++ RabbitMq.Publisher/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMq.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("logs", ExchangeType.Fanout);
+
+            var message = GetMessage(args);
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "logs",
+                                 routingKey: "",
+                                 basicProperties: null,
+                                 body: body);
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        }
+    }
+}
---
+++ RabbitMqTestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RabbitMqTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
